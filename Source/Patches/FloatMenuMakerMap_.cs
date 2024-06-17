c4eae26a8b6b483017179f12bdddeb86ac0eb5f1
AmCh-Q <AmCh-Q@users.noreply.github.com>
2024-06-17 17:21:35 -0400

    Check inside the code of action delegates when checking FloatMenuOptions

--- Source/Patches/FloatMenuMakerMap_.cs
+++ Source/Patches/FloatMenuMakerMap_.cs
@@ -39,53 +39,54 @@ namespace NeedBarOverflow.Patches.FloatMenuMakerMap_
 			}
 		}
 
+		private static readonly List<MethodInfo> targetOptionMethods
+			= GetMethodsUsingField(original, typeof(JobDefOf).Field(nameof(JobDefOf.Ingest)));
+		private static List<MethodInfo> GetMethodsUsingField(MethodBase method, FieldInfo targetField)
+		{
+			// Get children methods within the given method
+			// Such that all of the child methods use the given FieldInfo
+			List<MethodInfo> matchingMethods = [], unmatchingMethods = [];
+			foreach (object operand in PatchProcessor.ReadMethodBody(method).Select(x => x.Value))
+			{
+				// Skip if the operand is not a new method
+				if (operand is not MethodInfo methodCandidate
+					|| matchingMethods.Contains(methodCandidate)
+					|| unmatchingMethods.Contains(methodCandidate))
+				{
+					continue;
+				}
+				// Check if the new method uses the given FieldInfo
+				bool candidateMethodBody = PatchProcessor.ReadMethodBody(methodCandidate)
+					.Any(x => x.Value is FieldInfo field && field == targetField);
+				if (candidateMethodBody)
+					matchingMethods.Add(methodCandidate);
+				else
+					unmatchingMethods.Add(methodCandidate);
+			}
+			Debug.Message($"{matchingMethods.Count} method(s) found in method {method.Name} which uses Field {targetField.Name}.");
+			return matchingMethods;
+		}
+
 		private static void Postfix(
 			Vector3 clickPos, Pawn pawn, List<FloatMenuOption> opts)
 		{
 			if (Need_Food_.Utility.CanConsumeMoreFood(pawn))
 				return;
-			if (opts.All(opt => opt.action is null))
-				return;
-			IntVec3 c = IntVec3.FromVector3(clickPos);
-			if (c.ContainsStaticFire(pawn.Map))
-				return;
-
-			List<string> ingestOrders = [];
-			List<Thing> thingList = c.GetThingList(pawn.Map);
-			if (thingList.NullOrEmpty())
-				return;
-			foreach (Thing thing in thingList)
-			{
-				ThingDef thingDef = thing.def;
-				if (
-					!thingDef.IsNutritionGivingIngestible
-#if !v1_2 && !v1_3
-					|| thingDef.ingestible.specialThoughtDirect == ModDefOf.IngestedHemogenPack
-#endif
-					|| !pawn.RaceProps.CanEverEat(thing))
-				{
-					continue;
-				}
-
-				string ingestAction;
-				string ingestCommand = thingDef.ingestible.ingestCommandString;
-				if (ingestCommand.NullOrEmpty())
-					ingestAction = Strings.ConsumeThing.Translate(thing.LabelShort, thing);
-				else
-					ingestAction = ingestCommand.Formatted(thing.LabelShort);
-				ingestOrders.Add(ingestAction);
-			}
-			if (ingestOrders.Count == 0)
-				return;
-
 			foreach (FloatMenuOption opt in opts)
 			{
-				if (opt.action is null)
+				// Skip if the option is already disabled
+				// Skip if the option's action method does not match the candidate methods
+				if (opt.action is null || !targetOptionMethods.Contains(opt.action.Method))
 					continue;
-				string label = opt.Label;
-				if (ingestOrders.Any(label.StartsWith))
+				Thing thing = opt.revalidateClickTarget;
+				ThingDef thingDef = thing.def;
+				if (thingDef.IsNutritionGivingIngestible
+#if !v1_2 && !v1_3
+					&& thingDef.ingestible.specialThoughtDirect != ModDefOf.IngestedHemogenPack
+#endif
+					&& pawn.RaceProps.CanEverEat(thing))
 				{
-					opt.Label = string.Concat(label, ": ", Strings.FoodFull.Translate());
+					opt.Label = string.Concat(opt.Label, ": ", Strings.FoodFull.Translate());
 					opt.action = null;
 				}
 			}
