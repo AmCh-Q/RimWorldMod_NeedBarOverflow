0015de1dfb5bcc70cb890f5f4391b412f7c448ea
AmCh-Q <AmCh-Q@users.noreply.github.com>
2025-07-09 16:29:17 -0400

    Bump version to 1.3.3

--- Source/Main/Mod.cs
+++ Source/Main/Mod.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using UnityEngine;
 using Verse;
 
-[assembly: AssemblyVersionAttribute("1.3.2.0")]
+[assembly: AssemblyVersionAttribute("1.3.3.0")]
 
 namespace NeedBarOverflow
 {
